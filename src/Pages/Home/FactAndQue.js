2bb8465ed36241ec3d330a1294820299879cdc45
JunayetTamim <junayettamim186@gmail.com>
2022-05-22 02:47:37 +0600

    bonus 2 section add

--- src/Pages/Home/FactAndQue.js
+++ src/Pages/Home/FactAndQue.js
@@ -3,7 +3,23 @@ import React from 'react';
 const FactAndQue = () => {
     return (
         <div>
-            <h2>Bonus Sec F&Q </h2>
+            <h2 className='text-3xl bg-teal-400 rounded text-white p-2 text-center mb-10'>There is F&Q Section </h2>
+            <div tabIndex={0} class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div class="collapse-title text-xl font-medium">
+                    Focus me to see content
+                </div>
+                <div class="collapse-content">
+                    <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                </div>
+            </div>
+            <div tabIndex={0} class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+                <div class="collapse-title text-xl font-medium">
+                    Focus me to see content
+                </div>
+                <div class="collapse-content">
+                    <p>tabindex="0" attribute is necessary to make the div focusable</p>
+                </div>
+            </div>
         </div>
     );
 };
