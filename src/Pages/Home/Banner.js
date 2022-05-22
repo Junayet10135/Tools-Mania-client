bfbf4b227b9de3513bc3d2c0cfc78531294cb147
JunayetTamim <junayettamim186@gmail.com>
2022-05-22 12:34:42 +0600

    data load from mongodb

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import banner from '../../Assets/Images/banner.jpg'
+import bg from '../../Assets/Images/bg.png'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen ">
+        <div className="hero min-h-screen " style={{
+            backgroundImage: `url(${bg})`
+        }}>
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={banner} className="max-w-lg rounded-lg " alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Your New Smile Starts Here!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>   
+                    <h1 className="text-5xl font-bold"> Here Common Tools Useful for Working With Computers</h1>
+                     
                 </div>
             </div>
         </div>
